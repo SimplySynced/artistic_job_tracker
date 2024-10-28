81721759f9967f6b0163bb862d73653c6d03037e
Taylor Horwood <taylor.horwood1@gmail.com>
2024-10-27 21:53:01 -0400

    updating some structure

--- src/app/api/woods/route.js
+++ src/app/api/woods/route.js
@@ -18,3 +18,18 @@ export async function POST(request) {
 
   return new Response(JSON.stringify(newWoodType), { status: 201 });
 }
+
+export async function disable(request, { params }) {
+  const { id } = params;
+  const { wood_type } = await request.json();
+
+  try {
+    const updatedWoodType = await prisma.woodTypes.update({
+      where: { id: parseInt(id, 10) },
+      data: { wood_type },
+    });
+    return new Response(JSON.stringify(updatedWoodType), { status: 200 });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: "Failed to disable wood type" }), { status: 500 });
+  }
+}
